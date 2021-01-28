65ffa416e50541f78bcdd0daa3af1873e2cce834
Maciej Glowacki <m.glowacki01@gmail.com>
2021-01-28 19:54:13 +0100

    Almoust there with images

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,22 +1,21 @@
 import React from 'react';
-
-// Utils
-import { baseUrl } from '../../../config/server';
+// Components
+import Image from 'next/image';
 
 // Styles
 import classes from './Gallery.module.scss';
 
 const Gallery = ({ articles }) => {
-	console.log(articles);
-	return (
-		<section className={classes.columns}>
-			{articles.map((article) => (
-				<article key={article._id} className={classes.article}>
-					<img className={classes.image} src={baseUrl + article.picture.url} alt={article.title} />
-				</article>
-			))}
-		</section>
-	);
+	const images = articles.map((article) => (
+		<article key={article._id} className={classes.article}>
+			<Image layout="fill" className={classes.image} src={article.picture.url} alt={article.title} />
+		</article>
+	));
+	return <section className={classes.columns}>{images}</section>;
 };
 
 export default Gallery;
+
+// $medium-view: 650px;
+// $large-view: 960px;
+// $desktop-view: 1024px;
